cdb2840c4e787db00c3d59510385f1ae81f1bb68
Thomas Walker <thwalker@smcm.edu>
2020-11-17 20:29:40 -0500

    Moved everything off the /cdspec/ domain.

--- cd_spec_viewer_web/config/urls.py
+++ cd_spec_viewer_web/config/urls.py
@@ -6,7 +6,7 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    #path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
@@ -16,7 +16,7 @@ urlpatterns = [
     path("users/", include("cd_spec_viewer_web.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    path('cdspec/', include('cdspec.urls')),
+    path('', include('cdspec.urls', namespace="cdspec")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
