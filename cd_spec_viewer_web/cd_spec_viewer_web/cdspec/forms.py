6d99a54bcdabda20a2ffa5667653fc3039763ffc
jimtoledo <55601603+jimtoledo@users.noreply.github.com>
2021-02-22 16:01:33 -0500

    Updated specrun model and views/templates to work with other graph axes

--- cd_spec_viewer_web/cd_spec_viewer_web/cdspec/forms.py
+++ cd_spec_viewer_web/cd_spec_viewer_web/cdspec/forms.py
@@ -1,9 +1,7 @@
-from django import forms
 from django.forms import ModelForm
 from cd_spec_viewer_web.cdspec.models import SpecRun
 
-AXIS_CHOICES = [('nanometers', 'nm'), ('degrees', 'mdeg'), ('voltage', 'V'), ('absorbance', 'absorbance')]
-AXIS_CHOICES_WITH_NONE = AXIS_CHOICES + [('None', 'None')]
+
 
 #Upload form, model specifies which database table to use and fields specify the fields included in the form.
 class CreateForm(ModelForm):
@@ -16,8 +14,3 @@ class EditForm(ModelForm):
     class Meta:
         model = SpecRun
         fields = ('run_title', 'run_description', 'visible_student', 'visible_public')
-
-class AxesForm(forms.Form):
-    x_axis = forms.ChoiceField(label="X Axis", choices=AXIS_CHOICES, initial='nanometers')
-    y_axis = forms.ChoiceField(label="Y Axis", choices=AXIS_CHOICES, initial='degrees')
-    z_axis = forms.ChoiceField(label="Z Axis", choices=AXIS_CHOICES_WITH_NONE, initial='None')
