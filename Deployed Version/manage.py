41b2a56c1a340f3f1a18539d7054e6ba3cf7bd2e
AndrewK <79164095+ALKivrak@users.noreply.github.com>
2022-09-21 16:20:50 -0400

    Update manage.py

--- Deployed Version/manage.py
+++ Deployed Version/manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import os #imports operating system
-import sys #imports system
+import os
+import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cd_spec_viewer_web.settings")
