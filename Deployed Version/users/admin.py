b040d1dc94c8b37a2e2fddb8925f64b11a25b0cb
AndrewK <79164095+ALKivrak@users.noreply.github.com>
2022-09-26 15:26:02 -0400

    Update admin.py

--- Deployed Version/users/admin.py
+++ Deployed Version/users/admin.py
@@ -1,3 +1,4 @@
+#Commented by Andrew Kivrak
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
@@ -6,10 +7,14 @@ from django.utils.translation import ugettext as _
 from users.forms import UserChangeForm, UserCreationForm
 
 
+# Create a user model object with the name User and register User in the admin.
 User = get_user_model()
-
-
 @admin.register(User)
+
+# Create a UserAdmin class. This class will contain the interface operations to the User model. 
+
+# form -> Logout current User and login another User
+# add_form -> Add new User
 class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
@@ -18,26 +23,37 @@ class UserAdmin(auth_admin.UserAdmin):
         auth_admin.UserAdmin.fieldsets
     )
 
+    # Define the structure and fields of the user model.
     staff_fieldsets = (
         (None, {'fields': ("username", "is_staff")}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
         (_('Groups'), {'fields': ('groups',)}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-
+    
+    # Define which fields are to be made Read-Only to the staff in a tuple.
     staff_readonly_fields = ('name', 'first_name', 'last_name', 'email', 'last_login', 'date_joined')
-
+ 
+    # Define which fields are to be used while displaying list of staff in a list.
     list_display = ["username", "name", "is_superuser"]
+    
+    # Define which field can be used to search for a user in a list.
     search_fields = ["name"]
 
+    # Define a function get_fieldsets. This method will return data field values from the User Model.
     def get_fieldsets(self, request, obj=None):
+        # If the user is not a superuser, return the data of the user in staff_fieldsets.
         if not request.user.is_superuser:
             return self.staff_fieldsets
+        # If the user is a superuser, return the data of all users.
         else:
             return super(UserAdmin, self).get_fieldsets(request, obj)
 
+    # Define a function get_readonly_fields. This method will return all readonly data field values from the User Model.
     def get_readonly_fields(self, request, obj=None):
+        # If the user is not a superuser, return the readonly data of the user in staff_readonly_fields.
         if not request.user.is_superuser:
             return self.staff_readonly_fields
+        # If the user is a superuser, return the data of all users.
         else:
             return super(UserAdmin, self).get_readonly_fields(request, obj)
