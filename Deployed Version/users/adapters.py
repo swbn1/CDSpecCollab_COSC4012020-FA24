016de9f523ade93cbb84df54e46ea4c16c4ca270
AndrewK <79164095+ALKivrak@users.noreply.github.com>
2022-09-28 00:15:14 -0400

    Update adapters.py

--- Deployed Version/users/adapters.py
+++ Deployed Version/users/adapters.py
@@ -1,16 +1,28 @@
+#importing any method from typing package
 from typing import Any
 
+#importing the required files from other package/classes(user defined)
+
+#importing DefaultAccountAdapter from allauth.account.adapter
 from allauth.account.adapter import DefaultAccountAdapter
+
+#importing DefaultSocialAccountAdapter from allauth.socialaccount.adapter 
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+#importing settings form django.conf
 from django.conf import settings
+
+#importing HttpRequest from django.http 
 from django.http import HttpRequest
 
 
 class AccountAdapter(DefaultAccountAdapter):
+    #creating a method to return attributes settings, ACCOUNT_ALLOW_REGISTRATION from the Account
     def is_open_for_signup(self, request: HttpRequest):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
+    #creating a method to return the attributes settings, ACCOUNT_ALLOW_REGISTRATION from socialAccount
     def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
