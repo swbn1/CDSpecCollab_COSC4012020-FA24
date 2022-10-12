52179bb7b7343a0a1f000d79ed109b898b6f5247
snwahid <113939763+snwahid@users.noreply.github.com>
2022-10-12 15:29:44 -0400

    Update apps.py

--- Deployed Version/cdspec/apps.py
+++ Deployed Version/cdspec/apps.py
@@ -1,7 +1,10 @@
 #Django
 from django.apps import AppConfig
 
-#CDspecConfig is a subclass of AppConfig
+#'CDspecConfig' is a subclass of classs 'AppConfig'
 class CdspecConfig(AppConfig):
+
+   """Configures the CDspece app""" 
+
     name = 'cdspec'
 
