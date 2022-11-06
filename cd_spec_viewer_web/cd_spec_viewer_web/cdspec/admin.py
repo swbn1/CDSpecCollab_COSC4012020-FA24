c9e09160ff35d6ca090a81862657b6dff7e6c0a1
Swampysquid <91571049+Swampysquid@users.noreply.github.com>
2022-11-06 16:37:53 -0500

    Update admin.py

--- cd_spec_viewer_web/cd_spec_viewer_web/cdspec/admin.py
+++ cd_spec_viewer_web/cd_spec_viewer_web/cdspec/admin.py
@@ -1,5 +1,7 @@
 """Registers the admin model for the CDSpec app
+
 Prepared by COSC 401 as part of the CDSpec Viewer project for Dr. Sherrer
+
 This file and all contributions herin are covered by the GPL 3.0 License 
 https://www.gnu.org/licenses/gpl-3.0.html
 """
