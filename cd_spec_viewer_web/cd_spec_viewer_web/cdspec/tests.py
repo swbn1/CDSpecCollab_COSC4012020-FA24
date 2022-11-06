5daafd6aa41eb4b8173385b7ddc05e90608863e4
Swampysquid <91571049+Swampysquid@users.noreply.github.com>
2022-11-06 17:00:38 -0500

    Update tests.py

--- cd_spec_viewer_web/cd_spec_viewer_web/cdspec/tests.py
+++ cd_spec_viewer_web/cd_spec_viewer_web/cdspec/tests.py
@@ -1,3 +1,11 @@
+"""This file holds unit tests for the CDSpec 
+
+Prepared by COSC 401 as part of the CDSpec Viewer project for Dr. Sherrer
+
+This file and all contributions herin are covered by the GPL 3.0 License 
+https://www.gnu.org/licenses/gpl-3.0.html
+"""
+#Seemingly unused by last capstone
 from django.test import TestCase
 
 # Create your tests here.
