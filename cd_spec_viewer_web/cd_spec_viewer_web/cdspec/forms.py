c32dbd90d71bd7296d20246d84d34ca5def0d5fc
Swampysquid <91571049+Swampysquid@users.noreply.github.com>
2022-11-06 16:47:10 -0500

    Update forms.py

--- cd_spec_viewer_web/cd_spec_viewer_web/cdspec/forms.py
+++ cd_spec_viewer_web/cd_spec_viewer_web/cdspec/forms.py
@@ -1,4 +1,14 @@
+"""Implements the generic forms for viewing CDSpec models
+
+Prepared by COSC 401 as part of the CDSpec Viewer project for Dr. Sherrer
+
+This file and all contributions herin are covered by the GPL 3.0 License 
+https://www.gnu.org/licenses/gpl-3.0.html
+"""
+#Django
 from django.forms import ModelForm
+# DISCREPANCY NOTED: from cd_spec_viewer_web.cdspec.models here, but deployed folder had
+# from cdspec.models only
 from cd_spec_viewer_web.cdspec.models import SpecRun
 
 
@@ -9,7 +19,7 @@ class CreateForm(ModelForm):
         model = SpecRun
         fields = ('run_title', 'run_description', 'run_user', 'visible_student', 'visible_public',
          'source_file', 'protein_concentration', 'pathlength', 'number_of_amino_acids')
-
+#Edit Form, fields specify the fields that can be edited
 class EditForm(ModelForm):
     class Meta:
         model = SpecRun
