c2194d40a6f51003b10d1f7881a2d64993f5038c
William Capon <gardian299@gmail.com>
2023-04-10 15:28:50 -0400

    Put tarball as deployed version

--- cd_spec_viewer_web/config/settings/local.py
+++ cd_spec_viewer_web/config/settings/local.py
@@ -1,6 +1,5 @@
 from .base import *  # noqa
 from .base import env
-
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
@@ -26,9 +25,27 @@ CACHES = {
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = env(
-    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
-)
+#EMAIL_BACKEND = env(
+    #"DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+#)
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = "gmailapi_backend.mail.GmailBackend"
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
+EMAIL_TIMEOUT = 5
+# https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
+DEFAULT_FROM_EMAIL = "CD Spec Viewer Web <noreply@example.com>"
+# https://docs.djangoproject.com/en/dev/ref/settings/#server-email
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
+EMAIL_SUBJECT_PREFIX = "[CD Spec Viewer Web]"
+
+
+
+GMAIL_API_CLIENT_ID =""
+GMAIL_API_CLIENT_SECRET = ""
+GMAIL_API_REFRESH_TOKEN = ""
 
 # WhiteNoise
 # ------------------------------------------------------------------------------
