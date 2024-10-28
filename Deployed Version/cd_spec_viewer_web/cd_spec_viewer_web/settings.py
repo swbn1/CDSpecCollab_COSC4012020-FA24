5fe548732c879996b0c6644c14e8a1c40c356ec6
roman-h-p-spiwak <roman.h.p.spiwak@gmail.com>
2024-10-28 17:54:17 -0400

    Changes
    Removed the `.pyc`s and added parenthesis to the `print` statements in `oath2.py` (it didn't fix anything).

--- Deployed Version/cd_spec_viewer_web/cd_spec_viewer_web/settings.py
+++ Deployed Version/cd_spec_viewer_web/cd_spec_viewer_web/settings.py
@@ -172,7 +172,7 @@ X_FRAME_OPTIONS = "DENY"
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False #TODO This is only required for offline.
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
 SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
